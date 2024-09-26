325f3ad56b830c0fb371fe5f796bd329f2805b15
Andres Medina <am9226605@gmail.com>
2024-09-26 16:51:04 -0500

    Se crea el rpoyecyp con los ejemplos de los operadores

---
+++ operadoresProject/OperadoresAritmeticos.py
@@ -0,0 +1,21 @@
+#Ejemplos utilzando operadores aritmeticos
+numero1 = int(input("Ingrese el numero1:"))
+numero2 = int(input("Ingrese el numero2:"))
+
+suma = numero1 + numero2
+print(f"La suma {numero1} y {numero2} es: {suma}")
+
+resta = numero1 - numero2
+print(f"La resta {numero1} y {numero2} es: {resta}")
+
+division = numero1 / numero2
+print(f"La division {numero1} y {numero2} es: {division}")
+
+multiplicacion = numero1 * numero2
+print(f"La multiplicacion {numero1} y {numero2} es: {multiplicacion}")
+
+modulo = numero1 % numero2
+print(f"El modulo {numero1} y {numero2} es: {modulo}")
+
+divisionentera = numero1 // numero2
+print(f"La divisionentera {numero1} y {numero2} es: {divisionentera}")
